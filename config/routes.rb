54ed4346db95f17f78e9a8b9eca78393b18212c1
EC2 Default User <ec2-user@ip-172-31-30-229.ec2.internal>
2021-11-10 15:34:47 +0000

    インデントとルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,31 @@
 Rails.application.routes.draw do
-  get 'room/new'
-  get 'room/index'
+  get 'room/show'
+  post 'room/create'
   get 'room/posts'
   get 'room/search_room'
-  get 'room/show'
-  get 'users/show'
+  get 'room/search'
+  
+ post 'reserve/confirm'
+ post 'reserve/comprete'
+ post 'reserve/back' 
+ get 'reserve/reserved'
+ get 'search' => 'room#search'
+ 
+ get 'room/show'
+ 
  devise_for :users, controllers: { registrations: 'users/registrations' }
   get '/',to:'room#index'
   get 'reserve/room'
   get 'reserve/comprete'
   get 'reserve/confirm'
   get 'reserve/room'
+  
  devise_scope :users do
   post'update_profile',to:'users/registrations#update_profile'
   patch 'update_profile',to:'users/registrations#update_profile'
   end
-
- post 'room/create'
- post 'reserve/confirm'
- post 'reserve/comprete'
- post 'reserve/back' 
- get 'reserve/reserved'
- get 'search' => 'room#search'
- get 'room/search'
-
- resources :reserve
- resources :room
- resources :users
-  
-   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :reserve
+  resources :room
+  resources :users
 end
